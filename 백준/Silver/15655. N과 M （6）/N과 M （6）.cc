d6e3ae11676550e577f25821941309c3716a3d2f
Z1Y30N <50691701+hesedbaek@users.noreply.github.com>
2023-10-17 20:58:12 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int a, n, m;
+bool chk[10002];
+vector<int> v, ans;
+	
+void go(int k){
+	if(ans.size() == m){
+		for(int t:ans)	cout<<t<<" ";
+		cout<<'\n';
+		return;
+	}
+	
+	for(int i=k; i<n; i++){
+		ans.push_back(v[i]);
+		go(i+1);
+		ans.pop_back();
+	}
+}
+
+int main(){
+	fastio();
+	cin>>n>>m;
+
+	for(int i=0; i<n; i++){
+		cin>>a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	go(0);
+}
