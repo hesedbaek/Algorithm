022ee9a7e148c721523bb64051369b07aa40edb7
hesedbaek <50691701+hesedbaek@users.noreply.github.com>
2023-10-17 20:21:19 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int n, m;
+bool chk[10];
+vector<int> v;
+
+void go(int k){
+	if(v.size() == m){
+		for(int j:v)	cout<<j<<' ';
+		cout<<'\n';
+		return;
+	}
+	
+	for(int i=k; i<=n; i++){
+		v.push_back(i);
+		go(i+1);
+		v.pop_back();
+	}
+	return;
+}
+
+int main(){
+	fastio();
+	cin>>n>>m;
+	go(1);
+}
