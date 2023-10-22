71eb5578d7b5b368da2ecf613bfcea6ce096f83a
Z1Y30N <50691701+hesedbaek@users.noreply.github.com>
2023-10-22 23:59:07 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+string s;
+
+int main(){
+//	fastio();
+	while(getline(cin, s)){
+		cout<<s<<'\n';
+	}
+}
