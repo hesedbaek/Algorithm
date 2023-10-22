12a74581ea87fb9b5470836f2d6f4ad119a88c2b
Z1Y30N <50691701+hesedbaek@users.noreply.github.com>
2023-10-22 18:55:17 +0900

    [Gold IV] Title: 오큰수, Time: 248 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <stack>
 #include <cstring>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
-int n, a[1000002], ret[1000002];
-stack<int> s;
+int n, a[1000002], r[1000002];
+stack <int> s;
 
 int main(){
+	fastio();
 	cin>>n;
-	memset(ret, -1, sizeof(ret));
+	memset(r, -1, sizeof(r));
 	for(int i=0; i<n; i++){
 		cin>>a[i];
 		while(s.size() && a[s.top()] < a[i]){
-			ret[s.top()] = a[i];
+			r[s.top()] = a[i];
 			s.pop();
 		}
 		s.push(i);
-	}	
-	for(int i=0; i<n; i++)	cout<<ret[i]<<" ";
+	}
+	for(int i=0; i<n; i++)	cout<<r[i]<<" ";
 }
