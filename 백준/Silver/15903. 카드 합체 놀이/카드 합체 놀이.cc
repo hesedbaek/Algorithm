d7723174cc7576b51a2ba6b13db6f937b40b47bf
Z1Y30N <50691701+hesedbaek@users.noreply.github.com>
2023-11-08 19:54:23 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
+#include <queue>
 #define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 typedef long long ll;
 
 int n, m, a;
-multiset<ll> ms;
+priority_queue <ll, vector<ll>, greater<ll>> ms;
 ll sum;
 
 int main(){
 	fastio();
 	cin>>n>>m;
 	
-	for(int i=0; i<n; i++)	cin>>a, ms.insert(a);
+	for(int i=0; i<n; i++)	cin>>a, ms.push(a);
 
 	while(m--){
-		ll x = *ms.begin();
-		ms.erase(ms.begin());
-		x += *ms.begin();
-		ms.erase(ms.begin());
-		ms.insert(x);
-		ms.insert(x);
+		ll x = ms.top();
+		ms.pop();
+		x += ms.top();
+		ms.pop();
+		ms.push(x);
+		ms.push(x);
 	}
 	
-	for(ll i:ms)	sum+=i;
+	while(ms.size()){
+		sum+=ms.top();
+		ms.pop();
+	}	
 	cout<<sum;
 	
 
