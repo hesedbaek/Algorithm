841f8d1d9af6ae05d1ffaaf0e66c3fb296ef0211
Z1Y30N <50691701+hesedbaek@users.noreply.github.com>
2023-11-19 16:40:58 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main() {
+    fastio();
+	cout<<"       _.-;;-._"<<'\n';
+	cout<<"'-..-'|   ||   |"<<'\n';
+	cout<<"'-..-'|_.-;;-._|"<<'\n';
+	cout<<"'-..-'|   ||   |"<<'\n';
+	cout<<"'-..-'|_.-''-._|"<<'\n';
+}
